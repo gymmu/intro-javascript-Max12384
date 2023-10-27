428481f5339758119c66e6af39cfb5c2fbc58ee0
e282234 <e282234@sbl.ch>
2023-10-27 09:12:29 +0200

    Triangle

--- script.js
+++ script.js
@@ -18,3 +18,50 @@ function incrementCounter() {
   num = num + 1
   numberElement.textContent = num
 }
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
+}
+
+function resetCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = 0
+  numberElement.textContent = num
+}
+
+function writeText() {
+  const input = document.querySelector("#input")
+  const inputText = input.value
+  const output = document.querySelector("#output")
+  const outputBefore = output.textContent
+  output.textContent = outputBefore + "\n" + inputText
+}
+
+
+function Theorom() {
+  const input = document.querySelector("#inputA")
+  const inputText = input.value 
+  const inputB = document.querySelector("#inputB")
+  const inputBText = inputB.value 
+  const inputC = document.querySelector("#inputC")
+  const inputCText = inputC.value 
+  const output = document.querySelector("#outputA")
+  const output1 = document.querySelector("#outputB")
+  const output2 = document.querySelector("#outputC")
+  const squared = inputText * inputText + inputBText * inputBText + inputCText * inputCText
+  const root = Math.sqrt(squared)
+  output.textContent = root
+}
+
+function stoptext() {
+  disableTextbox.setAttribute('disabled', '');
+  disableTextbox.value = ''
+}
+function starttext() {
+  disableTextbox.removeAttribute('disabled', '');
+
+}
